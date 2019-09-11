add225bcc230e86ce640030cf005709f4acb7f0c
Hexagons <anton.heestand@gmail.com>
2019-09-11 16:46:07 +0200

    geos

---
+++ Source/GEOs/Content/SphereGEO.swift
@@ -0,0 +1,13 @@
+//
+//  SphereGEO.swift
+//  GEO3D
+//
+//  Created by Anton Heestand on 2019-08-19.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+import Foundation
+
+class SphereGEO: GEO {
+    
+}
---
+++ Source/GEOs/GEO.swift
@@ -0,0 +1,15 @@
+//
+//  GEO.swift
+//  GEO3D
+//
+//  Created by Anton Heestand on 2019-08-19.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+import Foundation
+
+class GEO {
+    
+//    let obj 
+    
+}
